ce844a6ab4c5723be1f89c9c74df1dfaf7a0c995
fatemeh khalili <fateme05khalili84@gmail.com>
2025-10-16 22:37:09 +0330

    feat:footer for desktop and mobile

--- src/layouts/PublicLayout/PublicLayout.tsx
+++ src/layouts/PublicLayout/PublicLayout.tsx
@@ -1,22 +1,24 @@
 import { Toaster } from "@/components/ui/sonner";
 import { Outlet } from "react-router-dom";
+import { Footer } from "@/components/Footer/Footer";
 
 const PublicLayout = () => {
-	return (
-		<>
-			<Toaster
-				position="bottom-right"
-				richColors={true}
-				duration={5000}
-				// toastOptions={{
-				// 	style: {
-				// 		color: "red",
-				// 	},
-				// }}
-			/>
-			<Outlet />
-		</>
-	);
+  return (
+    <>
+      <Toaster
+        position="bottom-right"
+        richColors={true}
+        duration={5000}
+        // toastOptions={{
+        // 	style: {
+        // 		color: "red",
+        // 	},
+        // }}
+      />
+      <Outlet />
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default PublicLayout;
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,63 +1,70 @@
 import { Button } from "@/components/Custom/Button/Button";
 import { Checkbox } from "@/components/Custom/Checkbox/Checkbox";
 import { Input } from "@/components/Custom/Input/Input";
-import { useDesktop, useDesktopTablet, useMobile, useTablet } from "@/hooks/ResponsiveHooks";
+import {
+  useDesktop,
+  useDesktopTablet,
+  useMobile,
+  useTablet,
+} from "@/hooks/ResponsiveHooks";
+import Footer from "@/components/Footer/Footer";
 import adjustInputDirection from "@/utils/adjustInputDirection";
+import PublicLayout from "@/layouts/PublicLayout/PublicLayout";
 
 function Test() {
-	function onChange(e: React.ChangeEvent<HTMLInputElement>) {
-		console.log(e.target.value);
-	}
-	const isDesktop = useDesktop();
-	const isMobile = useMobile();
-	const isTablet = useTablet();
-	return (
-		<div className="flex flex-col items-center">
+  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    console.log(e.target.value);
+  }
+  const isDesktop = useDesktop();
+  const isMobile = useMobile();
+  const isTablet = useTablet();
+  return (
+    <div className="flex flex-col items-center">
+      {isDesktop && <p> desktop mode</p>}
+      {isMobile && <p> mobile mode</p>}
+      {isTablet && <p> tablet mode</p>}
 
-			{isDesktop && <p> desktop mode</p>}
-			{isMobile && <p> mobile mode</p>}
-			{isTablet && <p> tablet mode</p>}
-
-			<Button shadow={true} size={"giant"} bold={true}>
-				ورود
-			</Button>
-			<br />
-			<br />
-			<Button
-				isLoading={true}
-				loadingSize={8}
-				shadow={true}
-				size={"giant"}
-				bold={true}
-			>
-				ورود
-			</Button>
-			<br />
-			<br />
-			<Input
-				className="w-80"
-				onChange={onChange}
-				shadow={true}
-				placeholder="ایمیل"
-			/>
-			<br />
-			<br />
-			<Input
-				className="w-80"
-				onChange={adjustInputDirection(onChange)}
-				shadow={true}
-				placeholder="ایمیل"
-			/>
-			<br />
-			<br />
-			<Checkbox text={"آقا عشق"} />
-			<br />
-			<br />
-			<Button variant={"link"} shadow={false} bold={true}>
-				فراموشی رمز عبور
-			</Button>
-		</div>
-	);
+      <Button shadow={true} size={"giant"} bold={true}>
+        ورود
+      </Button>
+      <br />
+      <br />
+      <Button
+        isLoading={true}
+        loadingSize={8}
+        shadow={true}
+        size={"giant"}
+        bold={true}
+      >
+        ورود
+      </Button>
+      <br />
+      <br />
+      <Input
+        className="w-80"
+        onChange={onChange}
+        shadow={true}
+        placeholder="ایمیل"
+      />
+      <br />
+      <br />
+      <Input
+        className="w-80"
+        onChange={adjustInputDirection(onChange)}
+        shadow={true}
+        placeholder="ایمیل"
+      />
+      <br />
+      <br />
+      <Checkbox text={"آقا عشق"} />
+      <br />
+      <br />
+      <Button variant={"link"} shadow={false} bold={true}>
+        فراموشی رمز عبور
+      </Button>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default Test;
