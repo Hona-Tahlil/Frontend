c08f63e14b0098baf61baf97cf972d3cb3fa9843
Sadra Sefidgari <sadrasefidgari@gmail.com>
2025-10-17 21:41:50 +0330

    chore: add authTypes for login

--- src/types/authTypes.ts
+++ src/types/authTypes.ts
@@ -1,14 +1,34 @@
 export interface LoginPayload {
 	email: string;
-	username: string | null;
 	password: string;
+	rememberMe: boolean;
 }
 
 export interface LoginResponse {
-	user: {
-		id: number;
-		name: string;
-		email: string;
-		username: string;
-	};
+	statusCode: number;
+	messages?: LoginResponseErrors;
+	message?: string;
+	data?: LoginResponseData;
+}
+
+export interface LoginResponseErrors {
+	email?: string;
+	password?: string;
+}
+
+export interface LoginResponseData {
+	accessToken: string;
+	role: RoleResponse;
+}
+
+export interface RoleResponse {
+	id: number;
+	name: string;
+	permissions: PermissionResponse[];
+}
+
+export interface PermissionResponse {
+	id: number;
+	name: string;
+	description: string;
 }
