1f6feb33cfa1b1a1745ec44b3d5668c9fdf590ba
Sadra Sefidgari <sadrasefidgari@gmail.com>
2025-10-20 16:52:52 +0330

    chore: remove unused imports

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,15 +2,6 @@ import AuthBackground from "@/components/Auth/Background/AuthBackground";
 
 import SignupForm from "@/components/Auth/SignupForm/SignupForm";
 
-import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
-} from "@/components/ui/dialog";
-
 export default function Signup() {
 	return (
 		<div className="w-screen h-screen bg-background overflow-hidden justify-center flex">
