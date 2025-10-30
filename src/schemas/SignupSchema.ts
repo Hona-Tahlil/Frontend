29b0c33577a7e9eaa56b2c041f10e2805b136815
Sadra Sefidgari <sadrasefidgari@gmail.com>
2025-10-30 15:38:42 +0330

    fix: update validators for signup schema

--- src/schemas/SignupSchema.ts
+++ src/schemas/SignupSchema.ts
@@ -1,7 +1,14 @@
 import * as Yup from "yup";
 
+const nameRegex = /^[A-Za-z\u0600-\u06FF\s]+$/;
+
 const validationSchema = Yup.object({
-	name: Yup.string().required("نام اجباری است"),
+	name: Yup.string()
+		.trim()
+		.matches(nameRegex, "نام معتبر نیست")
+		.min(2, "نام معتبر نیست.")
+		.max(50, "نام معتبر نیست")
+		.required("نام اجباری است"),
 	email: Yup.string()
 		.email("ایمیل معتبر نمی باشد")
 		.required("ایمیل اجباری است"),
