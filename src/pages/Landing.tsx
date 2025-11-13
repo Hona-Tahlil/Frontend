44c12c589f5fab205c6437ba84982281e3dcd66c
Sadra Sefidgari <sadrasefidgari@gmail.com>
2025-11-13 19:43:37 +0330

    feat: add base for article section

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,6 +2,7 @@ import Image1 from "@/assets/landing/Image1.png";
 import Image2 from "@/assets/landing/Image2.png";
 import Image3 from "@/assets/landing/Image3.png";
 import {
+	ChevronDown,
 	ChevronLeft,
 	Cross,
 	Dumbbell,
@@ -34,13 +35,13 @@ function Landing() {
 			className="w-full flex flex-col items-center justify-center mt-20"
 			dir="rtl"
 		>
-			<div className="flex flex-col items-center lg:items-stretch lg:flex-row w-full h-180 px-7">
+			<div className="flex flex-col items-center lg:items-stretch lg:flex-row w-full h-auto lg:h-180 px-7">
 				<div className="lg:w-1/2 flex justify-end">
-					<div className="w-150 flex flex-col items-center gap-1">
-						<div className="text-3xl text-center mt-5">
+					<div className="lg:w-150 flex flex-col items-center gap-1">
+						<div className="text-xl sm:text-3xl text-center mt-5">
 							پتیار، پلتفرم امن برای مراقبت از پت شما
 						</div>
-						<div className="text-3xl font-bold text-center">
+						<div className="text-xl sm:text-3xl font-bold text-center leading-20">
 							به سادگی پرستاری را در نزدیکی خود پیدا کنید، مستقیما چت کنید و با
 							اطمینان رزرو کنید.
 						</div>
@@ -57,7 +58,7 @@ function Landing() {
 					</div>
 				</div>
 				<div className="lg:w-1/2 flex">
-					<div className="w-160 flex items-center justify-center">
+					<div className="lg:w-160 flex items-center justify-center mt-0 lg:mt-0">
 						<img src={Image1} alt="Landing" className="w-auto h-auto" />
 					</div>
 				</div>
@@ -157,19 +158,19 @@ function Landing() {
 				</div>
 			</div>
 			<div className="w-full h-auto flex flex-col items-center px-8 mt-50">
-				<div className="flex w-full max-w-300">
-					<div className="flex justify-end w-1/2">
+				<div className="flex w-full flex-col lg:flex-row max-w-300">
+					<div className="flex justify-end lg:w-1/2">
 						<img src={Image2} className="w-full h-auto object-contain" />
 					</div>
-					<div className="flex justify-start w-1/2">
+					<div className="flex justify-start lg:w-1/2">
 						<div className="w-full flex flex-col items-start justify-center">
-							<div className="max-w-115 ">
+							<div className="lg:max-w-115 text-center lg:text-right">
 								<div className="w-full">
 									<p className="font-bold text-2xl">
 										توهم چالش پیدا کردن مراقب برای پت داری؟
 									</p>
 								</div>
-								<p className="text-justify mt-5">
+								<p className="mt-5 text-center lg:text-justify">
 									پیدا کردن پرستار مناسب برای پت، میتونه کلی استرس داشته باشه…
 									<br />
 									شاید مهمترین چیزی که مانعت میشه با خیال راحت سفر کنی یا ساعتها
@@ -181,17 +182,26 @@ function Landing() {
 					</div>
 				</div>
 			</div>
-			<div className="w-full h-auto flex flex-col items-center px-15 mt-50">
-				<div className="flex w-full max-w-300">
-					<div className="flex justify-end w-1/2">
-						<div className="max-w-150 flex flex-col items-end justify-center">
-							<div className="max-w-115 ">
+			<div
+				className="w-full h-auto flex flex-col items-center px-15 mt-50"
+				dir="ltr"
+			>
+				<div className="flex flex-col lg:flex-row w-full max-w-300">
+					<div className="flex justify-end lg:w-1/2">
+						<img src={Image3} className="w-full h-auto object-contain" />
+					</div>
+					<div className="flex justify-start lg:w-1/2">
+						<div className="w-full flex flex-col items-start justify-center">
+							<div
+								className="mt-20 lg:mt-0 lg:max-w-115 text-center lg:text-right"
+								dir="rtl"
+							>
 								<div className="w-full">
 									<p className="font-bold text-2xl">
 										آگاهانه پرستار مناسب رو پیدا کن.
 									</p>
 								</div>
-								<p className="text-justify mt-5">
+								<p className="text-center lg:text-justify mt-5">
 									ما پلتفرمی هستیم که به تو کمک میکنه در کمترین زمان، به جامعهای
 									از پرستاران تأییدشده و عاشق حیوانات دسترسی پیدا کنی. پرستارانی
 									که پروفایل، سوابق و نظرات کاربران قبلیشون رو میبینی و میتونی
@@ -199,13 +209,52 @@ function Landing() {
 									<br />
 									<br />
 									همین حالا جستجو رو شروع کن!
+									<br />
 								</p>
 								<Button className="px-7">جستجو</Button>
 							</div>
 						</div>
 					</div>
-					<div className="flex justify-start w-1/2">
-						<img src={Image3} className="w-full h-auto object-contain" />
+				</div>
+			</div>
+			<div className="flex items-center justify-center w-full h-auto lg:h-180 px-7">
+				<div className="flex flex-col w-150 h-full bg-secondary-200 rounded-3xl p-7">
+					<div className="w-full h-1/2 bg-secondary-900 rounded-3xl"></div>
+					<p className="font-bold text-3xl mt-2">عنوان</p>
+					<p className="text-xl mt-2">متن...</p>
+					<div className="w-full flex-1 flex items-end">
+						<Button shadow={false} className="bg-secondary-700 w-auto h-8 py-0">
+							مشاهده مقاله
+						</Button>
+					</div>
+				</div>
+				<div className="flex flex-col w-150 lg:h-180 px-7">
+					<div className="flex w-full">
+						<div className="w-1/2 flex flex-col gap-3">
+							<p className="text-2xl font-bold">مجله پتیار</p>
+							<p className="text-2xl">یادگیری بی حد مرز</p>
+						</div>
+						<div className="w-1/2 flex justify-end">
+							<p className="text-md">مشاهده همه ی مقالات</p>
+							<ChevronDown />
+						</div>
+					</div>
+					<div className="flex w-full h-1 rounded-full bg-secondary-900 mt-4"></div>
+					<div className="flex-1 w-full py-7 flex flex-col">
+						<div className="w-full h-auto flex gap-2">
+							<div className="size-40 bg-secondary-200 rounded-4xl"></div>
+							<div className="flex-1 py-4 flex flex-col">
+								<p className="text-lg font-bold">عنوان</p>
+								<p className="text-md">متن</p>
+								<div className="flex-1 flex items-end">
+									<p className="text-sm">مشاهده مقاله</p>
+								</div>
+							</div>
+							<div className="flex flex-col justify-end">
+								<p className="text-sm">تاریخ</p>
+								<p className="text-sm">...</p>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
