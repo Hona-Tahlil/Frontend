f8b5aad8df6988f52cfaaaaac6e8c87c27b16ca4
Sadra Sefidgari <sadrasefidgari@gmail.com>
2025-11-18 15:03:47 +0330

    fix: add missing loginService

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,9 +1,20 @@
 // src/services/authService.ts
 
-import type { SignupPayload, SignupResponse } from "@/types/authTypes";
+import type {
+	LoginResponse,
+	SignupPayload,
+	SignupResponse,
+} from "@/types/authTypes";
 import { postData } from "./services";
 
-// Login function
+export const loginService = async (
+	credentials: LoginPayload,
+): Promise<LoginResponse> => {
+	return postData({
+		endPoint: `/v1/auth/login`,
+		data: credentials,
+	});
+};
 
 export const signupService = async (
 	credentials: SignupPayload,
