c06de2f39d7acb3829d467a8cc8a098bcae5b92d
Sadra Sefidgari <sadrasefidgari@gmail.com>
2025-12-02 11:24:15 +0330

    feat: add BookingCard component both desktop and mobike

---
+++ src/components/Booking/PetOwner/BookingCard.tsx
@@ -0,0 +1,164 @@
+import { Button } from "@/components/Custom/Button/Button";
+import { Rating, RatingButton } from "@/components/Custom/Rating/Rating";
+import { useMobile } from "@/hooks/ResponsiveHooks";
+import { cn } from "@/lib/utils";
+import type { CardStatus, CommentStatus } from "@/types/bookingCardTypes";
+import { Calendar, Clock, MapPin } from "lucide-react";
+
+export default function BookingCard({
+	cardStatus,
+	commentStatus,
+}: {
+	cardStatus: CardStatus;
+	commentStatus: CommentStatus;
+}) {
+	const isMobile = useMobile();
+	return (
+		<div
+			className={cn(
+				"relative w-full border-3 sm:border-1 sm:border-black/20 rounded-3xl p-3 bg-white",
+				cardStatus == "rejected" ? "border-red-500" : "",
+				cardStatus == "accepted" ? "border-green-500" : "",
+				cardStatus == "done" ? "border-primary" : "",
+				cardStatus == "pending" ? "border-yellow-500" : "",
+			)}
+			dir="rtl"
+		>
+			{isMobile && (
+				<div className="absolute left-5 top-0 -translate-y-1/2 flex">
+					{cardStatus == "rejected" && (
+						<div className="border-0 bg-white rounded-none text-red-500 px-1">
+							رد شده
+						</div>
+					)}
+					{cardStatus == "accepted" && (
+						<div className="border-0 bg-white rounded-none text-green-500 px-1">
+							تایید شده
+						</div>
+					)}
+					{cardStatus == "done" && (
+						<div className="border-0 bg-white rounded-none text-primary px-1">
+							انجام شده
+						</div>
+					)}
+					{cardStatus == "pending" && (
+						<div className="border-0 bg-white rounded-none text-yellow-500 px-1">
+							در انتظار تایید
+						</div>
+					)}
+				</div>
+			)}
+			<div className="relative w-full h-full">
+				{!isMobile && (
+					<div className="absolute left-0 flex">
+						{cardStatus == "rejected" && (
+							<div className="border-2 border-red-500 text-red-500 rounded-full px-3">
+								رد شده
+							</div>
+						)}
+						{cardStatus == "accepted" && (
+							<div className="border-2 border-green-500 text-green-500 rounded-full px-3">
+								تایید شده
+							</div>
+						)}
+						{cardStatus == "done" && (
+							<div className="border-2 border-primary text-primary rounded-full px-3">
+								انجام شده
+							</div>
+						)}
+						{cardStatus == "pending" && (
+							<div className="border-2 border-yellow-500 text-yellow-500 rounded-full px-3">
+								در انتظار تایید
+							</div>
+						)}
+					</div>
+				)}
+
+				<div className="flex gap-5">
+					<div className="flex flex-col items-center">
+						<div className="rounded-full size-20 bg-gray-500"></div>
+						{isMobile && <p className="text-2xl">تست</p>}
+					</div>
+					<div className="flex-1 flex flex-col gap-2">
+						{!isMobile && <p className="text-2xl">تست</p>}
+						{!isMobile && <p className="text-xl">تست</p>}
+						<div className="flex flex-col items-center sm:flex-row gap-3 sm:gap-[5%] md:gap-[10%]">
+							<p className="flex text-gray-500 gap-1">
+								<MapPin />
+								تهران و نیاوران
+							</p>
+							<p className="flex text-gray-500 gap-1">
+								<Calendar />
+								1403/08/20
+							</p>
+							<p className="flex text-gray-500 gap-1">
+								<Clock />
+								تهران و نیاوران
+							</p>
+						</div>
+						<p className="text-xl w-full text-left">تست</p>
+						<div className="w-full flex justify-end gap-2">
+							<Button
+								className="shadow-none h-8 py-0 !opacity-0"
+								disabled
+							></Button>
+							{cardStatus == "accepted" && (
+								<Button shadow={false} className="shadow-none h-8 py-0">
+									چت
+								</Button>
+							)}
+							{cardStatus != "rejected" && cardStatus != "canceled" && (
+								<Button
+									shadow={false}
+									variant={"outline"}
+									className="shadow-none h-8 py-0"
+								>
+									مشاهده جزئیات
+								</Button>
+							)}
+							{cardStatus == "accepted" && (
+								<Button
+									variant={"outline"}
+									shadow={false}
+									className="shadow-none h-8 py-0 text-red-500 border-red-500 hover:bg-red-500"
+								>
+									لغو رزرو
+								</Button>
+							)}
+							{cardStatus == "pending" && (
+								<Button
+									variant={"outline"}
+									shadow={false}
+									className="shadow-none h-8 py-0 text-red-500 border-red-500 hover:bg-red-500"
+								>
+									اغو درخواست
+								</Button>
+							)}
+							{cardStatus == "done" && !commentStatus && (
+								<Button shadow={false} className="shadow-none h-8 py-0">
+									ثبت نظر
+								</Button>
+							)}
+							{cardStatus == "done" && commentStatus && (
+								<>
+									<div className="flex items-center gap-2">
+										<p className="text-md">نظر شما:</p>
+										<Rating defaultValue={commentStatus} readOnly>
+											{Array.from({ length: 5 }).map((_, index) => (
+												<RatingButton
+													className="text-primary"
+													key={index}
+													size={10}
+												/>
+											))}
+										</Rating>
+									</div>
+								</>
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ src/types/bookingCardTypes.ts
@@ -0,0 +1,7 @@
+export type CardStatus =
+	| "canceled"
+	| "rejected"
+	| "done"
+	| "accepted"
+	| "pending";
+export type CommentStatus = 0 | 1 | 2 | 3 | 4 | 5 | undefined;
