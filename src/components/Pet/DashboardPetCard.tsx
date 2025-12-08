c6271a46012bb078af31c449dc8998f71674424e
MohammadAminBahari <bahariamin.1384@gmail.com>
2025-12-08 19:04:59 +0330

    feat: add mobile version of the pet card

--- src/components/Pet/DashboardPetCard.tsx
+++ src/components/Pet/DashboardPetCard.tsx
@@ -12,7 +12,9 @@ import { Eye, Mars, Trash, Venus } from "lucide-react";
 import { FaMale } from "react-icons/fa";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import petProfile from "@/assets/images/pet-temp.jpg";
+import petDefault from "@/assets/images/pet-profile-bg.png";
 import petBgPatter from "@/assets/images/paw-pattern-bg.png";
+import { useMobile } from "@/hooks/ResponsiveHooks";
 
 type DashboardPetCardProps = {
   name: string;
@@ -21,6 +23,8 @@ type DashboardPetCardProps = {
   age: string | null;
   isAdult: boolean | null;
   gender: string;
+  id: number;
+  pictureLink?: string | undefined;
 };
 
 export default function DashboardPetCard({
@@ -30,16 +34,19 @@ export default function DashboardPetCard({
   age,
   isAdult,
   gender,
+  id,
+  pictureLink,
 }: DashboardPetCardProps) {
+  const isMobile = useMobile();
   return (
-    <Card className="rounded-lg border-none rtl mb-3">
+    <Card className="rounded-lg border-none rtl mb-3 w-fit">
       <CardHeader className="p-0 flex">
         <CardTitle
-          className="h-25 rounded-t-lg bg-cover "
+          className="h-13 md:h-25 rounded-t-lg bg-cover "
           style={{ backgroundImage: `url(${petBgPatter})` }}
         ></CardTitle>
         <div className="h-fit w-fit flex justify-center items-center self-center">
-          <Avatar className="-mt-[55%]  w-25 h-25">
+          <Avatar className=" -mt-[60%] md:-mt-[55%] w-15 h-15  md:w-25 md:h-25">
             <AvatarImage
               src={petProfile}
               className="object-cover"
@@ -47,26 +54,42 @@ export default function DashboardPetCard({
           </Avatar>
         </div>
       </CardHeader>
-      <CardContent className="">
+      <CardContent className="px-3 mb-0 pb-2">
         <div className="flex justify-between">
-          <p className="text-lg font-bold">{name}</p>
-          {gender === "male" ? <Mars /> : <Venus />}
+          <p className="text-sm md:text-lg font-bold">{name}</p>
+          {gender === "male" ? <Mars size={isMobile ? 20 : 32} /> : <Venus size={isMobile ? 20 : 32}/>}
         </div>
-        <p className="text-sm mt-2">
+        <p className="text-xs md:text-sm mt-2">
           {kind} {species}
         </p>
-        <p className="text-sm">{age} سال</p>
+        <p className="text-xs md:text-sm">{age} سال</p>
       </CardContent>
-      <CardFooter className="flex gap-2 mt-5">
-        <Button variant={"outline"} className="shadow-none drop-shadow-none">
-          <Trash />
-          حذف
-        </Button>
-        <Button className="shadow-none drop-shadow-none">
-          <Eye />
-          مشاهده
-        </Button>
-      </CardFooter>
+      {isMobile ? (
+        <CardFooter className="flex flex-col md:mt-5 px-3 pb-2">
+          <Button className="shadow-none drop-shadow-none w-full h-7 text-xs">
+            <Eye />
+            مشاهده
+          </Button>
+          {/* <Button
+            variant={"outline"}
+            className="shadow-none drop-shadow-none w-full h-7 text-xs"
+          >
+            <Trash />
+            حذف
+          </Button> */}
+        </CardFooter>
+      ) : (
+        <CardFooter className="flex gap-2 mt-1 md:mt-5">
+          <Button variant={"outline"} className="shadow-none drop-shadow-none">
+            <Trash />
+            حذف
+          </Button>
+          <Button className="shadow-none drop-shadow-none">
+            <Eye />
+            مشاهده
+          </Button>
+        </CardFooter>
+      )}
     </Card>
   );
 }
