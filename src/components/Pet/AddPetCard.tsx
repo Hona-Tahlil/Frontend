c06951268b60f65cd69955b940975057a8cf7f34
MohammadAminBahari <bahariamin.1384@gmail.com>
2025-12-21 10:35:25 +0330

    fix: fix the problem with adding pet and showing it in dashboard pet page

--- src/components/Pet/AddPetCard.tsx
+++ src/components/Pet/AddPetCard.tsx
@@ -1,6 +1,6 @@
 import { useMobile } from "@/hooks/ResponsiveHooks";
 import { Plus } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import PetRegisterForm from "../PetRegister/PetRegisterForm";
 import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
@@ -8,43 +8,39 @@ import { isMotionComponent } from "framer-motion";
 
 function CardStyle() {
   return (
-    <div
-      className="border-4 border-primary border-dotted w-full h-80 bg-primary-200 flex-col rounded-lg flex justify-center items-center text-primary hover:cursor-pointer"
-    >
+    <div className="border-4 border-primary border-dotted w-full h-80 bg-primary-200 flex-col rounded-lg flex justify-center items-center text-primary hover:cursor-pointer">
       <Plus size={70} strokeWidth={2} />
       <p className="text-2xl font-bold mt-4">اضافه کردن پت</p>
     </div>
   );
 }
 
-
-
-
 export default function AddPetCard() {
   const isMobile = useMobile();
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
 
-  if (isMobile) {
-    return (
-      <div
-        onClick={() => {
-          navigate("/RegisterPet");
-          window.scrollTo(0, 0);
-        }}
-      >
-        <CardStyle />
-      </div>
-    );
-  }
+  const handleOpen = () => {
+    if (isMobile) {
+      navigate("/RegisterPet");
+      window.scrollTo(0, 0);
+    } else {
+      setOpen(true);
+    }
+  };
 
   return (
-    <Dialog>
-      <DialogTrigger asChild>
+    <>
+      <div onClick={handleOpen}>
         <CardStyle />
-      </DialogTrigger>
-      <DialogContent className="w-200 h-[90%]" dir="rtl">
-        <PetRegisterForm />
-      </DialogContent>
-    </Dialog>
+      </div>
+
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="w-200 h-[90%]" dir="rtl">
+          <PetRegisterForm closeModal={() => setOpen(false)} />
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
+
