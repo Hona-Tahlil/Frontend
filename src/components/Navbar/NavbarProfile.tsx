22d7b0b5129935c4d77abb6f15d27a644a09a3b8
MohammadAminBahari <84309493+MohammadAminBahari@users.noreply.github.com>
2025-12-30 21:40:40 +0330

    Fix/navbar (#75)
    * feat: add first name and last name and email to zustand storage and mock service for navbar
    
    * fix: finish the issue with user being logged in and routing

--- src/components/Navbar/NavbarProfile.tsx
+++ src/components/Navbar/NavbarProfile.tsx
@@ -8,11 +8,16 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-
   DropdownMenuTrigger,
 } from "@/components/Custom/Dropdonw-Menu/DropdownMenu";
+import useUserStore from "@/store/userStore/userStore";
+import { useNavigate } from "react-router-dom";
 
 export default function NavbarProfile() {
+  const { firstName, lastName, email } = useUserStore();
+  const { logout } = useUserStore();
+  const navigate = useNavigate();
+
   const userImage = "";
   return (
     <div className="h-full flex items-center gap-0.5 text-right lg:ml-7">
@@ -42,23 +47,35 @@ export default function NavbarProfile() {
           sideOffset={8}
         >
           <DropdownMenuLabel className="mt-2">
-            محمد امین بهاری
+            {firstName} {lastName}
           </DropdownMenuLabel>
           <DropdownMenuLabel className="text-xs font-light">
-            bahariamin.1384@gmail.com
+            {email}
           </DropdownMenuLabel>
           <DropdownMenuGroup>
-            <DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer" onClick={() => {navigate("/Dashboard/pets")}}>
+              <Heart />
+              داشبورد صاحب پت
+            </DropdownMenuItem>
+
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => {
+                navigate("/test");
+              }}
+            >
               <PawPrint />
               داشبورد پت‌یار
             </DropdownMenuItem>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <Heart />
-              داشبورد صاحب پت
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => {
+                logout();
+                navigate("/")
+              }}
+            >
               <LogOut />
               خروج از حساب
             </DropdownMenuItem>
