c79887825520791e28fec4e44f00bc0c90099b5c
MohammadAminBahari <bahariamin.1384@gmail.com>
2026-01-03 09:29:35 +0330

    fix: fix the problem with dialog box layout ui

--- src/components/RequestDialogboxLayout/RequestDialogboxLayout.tsx
+++ src/components/RequestDialogboxLayout/RequestDialogboxLayout.tsx
@@ -35,11 +35,10 @@ export default function RequestDialogboxLayout({
 
       <DialogContent
         className="
-          p-0 overflow-hidden
-          w-200 rounded-2xl rtl
+         rounded-2xl rtl   w-[40%] max-w-none !p-0
         "
       >
-        <div className="relative bg-[#A8D0D5] h-20 flex items-center justify-center">
+        <div className="relative bg-[#A8D0D5] h-25 flex items-center justify-center w-[100%]">
           <DialogClose className="absolute left-4 top-1/2 -translate-y-1/2 hover:cursor-pointer">
             <div className="aspect-square rounded-full w-8 h-8 border-2 flex items-center justify-center border-teal-600">
               <X className="w-5 h-5 text-teal-600" />
@@ -49,14 +48,14 @@ export default function RequestDialogboxLayout({
 
         <div className="flex ">
           <div className="w-fit h-fit justify-end pr-6 flex flex-col items-center">
-            <Avatar className="w-24 h-24 border-4 -mt-[50%] border-teal-600 shadow-md">
+            <Avatar className="w-24 h-24 border-4 -mt-[60%] border-teal-600 shadow-md">
               <AvatarImage src={petsitterImage} alt={petsitterName} />
               <AvatarFallback>{initials}</AvatarFallback>
             </Avatar>
             <div className="font-bold text-lg mt-2 ">{petsitterName}</div>
           </div>
 
-          <div className="px-6 pb-6 w-full">{children}</div>
+          <div className="px-10 pb-15 w-full">{children}</div>
         </div>
       </DialogContent>
     </Dialog>
